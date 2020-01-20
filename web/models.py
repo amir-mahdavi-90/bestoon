9cd7ec9ddcd2b1c694661da3cc422e5bc62db8b6
amir <amahdavi3230@gmail.com>
2020-01-20 19:39:19 +0330

    income in models and some beatification

--- web/models.py
+++ web/models.py
@@ -10,3 +10,12 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
